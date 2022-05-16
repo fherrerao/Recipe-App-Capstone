520eb925120fcb5618091e42af61f610eadcefbc
fherrerao <fherrera0206@gmail.com>
2022-05-16 16:17:35 -0500

    Add validations to name and make to the email confirmeable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :recipes
   has_many :foods
+
+  validates :name, uniqueness: { case_sensitive: false }, 
+  presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
 end
