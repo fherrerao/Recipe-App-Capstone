e66cda2ba83796e30c57807b6cbd27112ddee7b6
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-16 13:54:18 -0600

    Add seeds for the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.new(name: 'Billy')
+# , email: 'first@email.com', :password => '123456', :password_confirmation => '123456')
+# first_user.skip_confirmation!
+first_user.save!
+second_user = User.new(name: 'Bob')
+# , email: 'second@email.com', :password => '123456', :password_confirmation => '123456')
+# second_user.skip_confirmation!
+second_user.save!
+
+first_food = Food.create!(name: 'Rice', measurement_unit: 'grams', price: 1.2, user: first_user)
+second_food = Food.create!(name: 'Beans', measurement_unit: 'grams', price: 2.3, user: first_user)
+third_food = Food.create!(name: 'Oil', measurement_unit: 'millilitters', price: 0.6, user: first_user)
+
+fourth_food = Food.create!(name: 'Spaghetti', measurement_unit: 'grams', price: 2.3, user: second_user)
+fifth_food = Food.create!(name: 'Tomato', measurement_unit: 'grams', price: 1.1, user: second_user)
+sixth_food = Food.create!(name: 'Salt', measurement_unit: 'grams', price: 4.2, user: second_user)
