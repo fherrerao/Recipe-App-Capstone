bfef87937fb5d3d0f930e4ab0617c6393bf0aa42
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-17 16:10:47 -0600

    Enable destroy action for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,22 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :destroy]
+
   def index 
     @recipes = Recipe.all
   end
 
   def show; end
+
+  def destroy
+    @recipe.destroy
+
+    flash[:success] = 'Post has been deleted successfully'
+    redirect_to recipes_url
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 end
