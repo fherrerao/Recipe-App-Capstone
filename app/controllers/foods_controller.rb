0214635889ee32613b4b87dca4da4f37251da51e
fherrerao <fherrera0206@gmail.com>
2022-05-17 16:01:15 -0500

    Add user to the food and permit params from the form

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -21,7 +21,7 @@ class FoodsController < ApplicationController
   # POST /foods or /foods.json
   def create
     @food = Food.new(food_params)
-
+    @food.user = current_user
     respond_to do |format|
       if @food.save
         format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
@@ -65,6 +65,6 @@ class FoodsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def food_params
-    params.fetch(:food, {})
+    params.fetch(:food, {}).permit(:name, :measurement_unit, :price)    
   end
 end
