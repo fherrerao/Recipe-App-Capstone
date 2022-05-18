0ef4831a0bb6597da9c152e95fdc6081f6781b9f
fherrerao <fherrera0206@gmail.com>
2022-05-18 11:46:03 -0500

    Change the query to fix bug with delete RecipeFood

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,17 +1,20 @@
 class RecipeFoodsController < ApplicationController
+  before_action :set_recipe_food, only: %i[show edit update destroy]
+  load_and_authorize_resource
+
   def create; end
 
   def destroy
     @recipe_food.destroy
 
     flash[:success] = 'Recipe food has been deleted successfully'
-    redirect_to recipe_url(@recipe_food.recipe_id)
+    redirect_to recipe_url(@recipe.id)
   end
 
   private
 
   def set_recipe_food
     @recipe = Recipe.find(params[:recipe_id])
-    @recipe_food = @recipe.foods.find(params[:id])
+    @recipe_food = RecipeFood.find(params[:id])
   end
 end
