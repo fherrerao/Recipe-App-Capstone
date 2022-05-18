e1a48052d2288040969631549f08550fd1b4c556
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-17 20:18:04 -0600

    Enable user account cancelation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :recipes
-  has_many :foods
+  has_many :recipes, dependent: :destroy
+  has_many :foods, dependent: :destroy
 
   validates :name, uniqueness: { case_sensitive: false },
                    presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
