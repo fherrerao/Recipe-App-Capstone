9f98b87d7c98fd4bfc1ca053e60a039c0a4628fc
fherrerao <fherrera0206@gmail.com>
2022-05-19 12:55:24 -0500

    Fix linter errors

--- app/helpers/general_shopping_list_helper.rb
+++ app/helpers/general_shopping_list_helper.rb
@@ -1,10 +1,12 @@
 module GeneralShoppingListHelper
+  # rubocop:disable Style/ClassVars
   @@shopping = []
   def getting_shopping
     @@shopping
   end
 
-  def add_shopping    
+  def add_shopping
     @@shopping.push(*Recipe.find(params[:recipe]).recipe_foods)
   end
+  # rubocop:enable Style/ClassVars
 end
