b09778156f25ab143eb109c0b97f4055e57c2ec8
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-19 17:09:33 -0600

    Show reduced description in recipes#index

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,10 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
+
+  def reduced_description
+    return description if description.length < 100
+
+    "#{description[0, 100]}..."
+  end
 end
