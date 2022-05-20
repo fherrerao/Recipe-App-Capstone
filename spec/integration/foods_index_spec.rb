c8ce2074f4ba13c9e936ffab1f1390a0ad0efbb5
fherrerao <fherrera0206@gmail.com>
2022-05-19 19:12:48 -0500

    Fix linter errors

--- spec/integration/foods_index_spec.rb
+++ spec/integration/foods_index_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
-RSpec.describe 'Food', type: :system do 
+RSpec.describe 'Food', type: :system do
   before do
-    User.destroy_all    
-    @user = User.new(name: 'Billy', email: 'first@email.com', :password => '123456', :password_confirmation => '123456')
+    User.destroy_all
+    @user = User.new(name: 'Billy', email: 'first@email.com', password: '123456', password_confirmation: '123456')
     @user.skip_confirmation!
     @user.save
 
@@ -11,37 +11,36 @@ RSpec.describe 'Food', type: :system do
     @food.save
   end
 
-  it 'to visit the foods page you need to sign in first' do        
-    visit foods_path         
+  it 'to visit the foods page you need to sign in first' do
+    visit foods_path
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 
   describe 'show list of foods' do
-    before:each do 
-      visit new_user_session_path     
+    before :each do
+      visit new_user_session_path
       fill_in 'Email', with: 'first@email.com'
       fill_in 'Password', with: '123456'
       click_button 'Log in'
     end
 
-  
-    it 'should display the list of foods' do        
+    it 'should display the list of foods' do
       expect(page).to have_content 'Foods'
     end
-  
-    it 'should delete a food' do  
+
+    it 'should delete a food' do
       click_button 'Delete food'
       expect(page).to have_content 'Food was successfully destroyed.'
     end
-  
+
     it "should display the foods's name" do
       expect(page).to have_text('Rice')
     end
-  
+
     it "should display the food's measurement_unit" do
       expect(page).to have_text('grams')
     end
-  
+
     it 'should have a new food button' do
       expect(page).to have_link('New food')
     end
--- spec/integration/foods_new_spec.rb
+++ spec/integration/foods_new_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Foods new', type: :system do
   before do
-    User.destroy_all    
-    @user = User.new(name: 'Billy', email: 'first@email.com', :password => '123456', :password_confirmation => '123456')
+    User.destroy_all
+    @user = User.new(name: 'Billy', email: 'first@email.com', password: '123456', password_confirmation: '123456')
     @user.skip_confirmation!
     @user.save
   end
 
   describe 'Add new food' do
-    before:each do
+    before :each do
       visit new_food_path
       fill_in 'Email', with: 'first@email.com'
       fill_in 'Password', with: '123456'
-      click_button 'Log in' 
+      click_button 'Log in'
     end
 
     it 'should create new food' do
