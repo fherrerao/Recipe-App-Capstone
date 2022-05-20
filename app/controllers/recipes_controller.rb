878896e549321522a052e1dfd1af71d38142db68
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-19 21:17:56 -0600

    Remove unused preloading in queries

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -86,7 +86,7 @@ class RecipesController < ApplicationController
   private
 
   def set_recipe
-    @recipe = Recipe.includes(:recipe_foods, recipe_foods: %i[food]).find(params[:id])
+    @recipe = Recipe.find(params[:id])
   end
 
   def recipe_params
@@ -102,6 +102,6 @@ class RecipesController < ApplicationController
   end
 
   def set_other_recipe
-    @recipe = Recipe.includes(:recipe_foods, recipe_foods: %i[food]).find(params[:recipe_id])
+    @recipe = Recipe.find(params[:recipe_id])
   end
 end
