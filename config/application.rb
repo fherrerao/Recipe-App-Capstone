5a5e0ec93fb5ea47495d30c2b9537bf0f431d50f
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-20 18:57:12 -0600

    Disable precompile assets for production heroku

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module RecipeApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
